10db6e027f7fbc995c22db82d2af182ae34b0900
Ben Wallace <ben@MacBook-Pro.phi.lup.ie>
2020-03-30 15:08:32 -0400

    1585595312-autosave

---
+++ resources/js/11-134.js
@@ -0,0 +1,33 @@
+var p1 = -10;
+
+while (p1 < 20) {
+  console.log(p1);
+  p1++;
+}
+
+var p2 = 10
+
+while (p2 <= 40) {
+  if (p2 % 2 === 0) {
+    console.log(p2);
+  }
+  p2++;
+}
+
+var p3 = 300;
+
+while (p3 <= 333) {
+  if (p3 % 2 === 1) {
+    console.log(p3);
+  }
+  p3++;
+}
+
+var p4 = 5;
+
+while (p4 <= 50) {
+  if (p4 % 5 === 0 && p4 % 3 === 0){
+    console.log(p4);
+  }
+  p4++;
+}
