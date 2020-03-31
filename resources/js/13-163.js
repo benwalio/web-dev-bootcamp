3944331df53d4fee96ee76b1947808659335b9eb
Ben Wallace <ben@benwal.io>
2020-03-30 21:01:14 -0400

    1585616474-autosave

--- resources/js/13-163.js
+++ resources/js/13-163.js
@@ -16,9 +16,25 @@ function isUniform(array) {
 }
 
 function sumArray(array) {
-  
+  let sum = 0;
+  for (let item of array) {
+    sum += item;
+  }
+  return sum;
+}
+
+function max(array) {
+  let max = 0;
+  for (let item of array) {
+    if (item > max) {
+      max = item;
+    }
+  }
+  return max;
 }
 
 printReverse(["hello","henlo","bye","gbyez"]);
 console.log(isUniform([1,1,1,1,1,1,1]));
 console.log(isUniform([1,2,1,1,2,2,1,1,2,1]));
+console.log(sumArray([1,2,3]));
+console.log(max([1,2,3,6666,3,2,1]));
