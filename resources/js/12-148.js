baf3854080c7b358fda5ab827e3bf754ac7502de
Ben Wallace <ben@benwal.io>
2020-03-30 20:34:41 -0400

    1585614881-autosave

--- resources/js/12-148.js
+++ resources/js/12-148.js
@@ -6,4 +6,35 @@ function isEven(x) {
   }
 }
 
-console.log(isEven(2));
+function factorial(x) {
+  let product = 0;
+  if (x > 0) {
+    product = x;
+    x--;
+    for (let i = x - 1; i > 0; i--) {
+      product = product * x;
+      x--;
+    }
+  } else if (x < 0) {
+    x *= -1;
+    console.log(x);
+    product = x;
+    x--;
+    for (let i = x - 1; i > 0; i--) {
+      product = product * x;
+      x--;
+    }
+    product *= -1
+  } else {
+    product = 1;
+  }
+  return product;
+}
+
+function kebabToSnake(variable) {
+  return variable.replace(/-/g, "_");
+}
+
+console.log(isEven(50));
+console.log(factorial(10));
+console.log(kebabToSnake("haha-haha-haha-oh"));
