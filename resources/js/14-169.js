3f0771a7cc65eb87aa67d9d2449e193db87cbe2d
Ben Wallace <ben@benwal.io>
2020-03-31 11:30:54 -0400

    1585668654-autosave

---
+++ resources/js/14-169.js
@@ -0,0 +1,11 @@
+var someObj = {
+  friends: [
+    {name: "Malfoy"},
+    {name: "crabbe"},
+    {name: "hoyle"}
+  ],
+  color: "baby blue",
+  isEvil: true
+};
+
+console.log(someObj.friends[0].name);
---
+++ resources/js/14-170.js
@@ -0,0 +1,4 @@
+var movieArray = [
+  {name: "frozen", rating: 5, hasWatched = true},
+
+]
