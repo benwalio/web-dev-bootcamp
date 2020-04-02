36313ceb953293fa48b2dff81db7a2ced2433b57
Ben Wallace <ben@MacBook-Pro.phi.lup.ie>
2020-04-02 10:59:53 -0400

    1585839593-autosave

--- resources/js/20-221.js
+++ resources/js/20-221.js
@@ -1,10 +1,27 @@
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("done");
 });
 
-$("li span").on("click", function (event) {
+$("ul").on("click", "li span", function (event) {
   $(this).parent().fadeOut(function () {
     $(this).remove();
   });
   event.stopPropagation();
-})
+});
+
+$("#create-new-todo").on("keypress", (function(event) {
+  /* Act on the event */
+  let key = event.which;
+  if (key == 13) {
+    if ($(this).val() !== "") {
+      let value = $(this).val();
+      $("#container .tasks ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + value + "</li>");
+      $(this).val("");
+    }
+  }
+}));
+
+$("h1 i").on("click", function () {
+  $("#create-new-todo").slideToggle();
+  $("#create-new-todo").is(":visible") ? "Collapse" : "Expand";
+});
