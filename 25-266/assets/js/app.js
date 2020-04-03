e822c1aba2293aa8ee95f7014becf9dc0ef4c41e
Ubuntu <ubuntu@ip-172-31-32-5.us-east-2.compute.internal>
2020-04-03 13:36:09 +0000

    1585920969-autosave

--- 25-266/assets/js/app.js
+++ 25-266/assets/js/app.js
@@ -1,10 +1,14 @@
 var express = require("express");
-var app = express;
+var app = express();
 
 app.get("/", function (req, res) {
   res.send("henlo there");
 });
 
+app.get("/bye", function (req, res) {
+  res.send("gbye");
+})
+
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log("server started");
+  console.log("server started " + process.env.IP + ":" + process.env.PORT);
 });
