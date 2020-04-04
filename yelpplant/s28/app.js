f60c39b8b70a7c6fbdd79cc3ca1898cf1e7e3129
Ubuntu <ubuntu@ip-172-31-32-5.us-east-2.compute.internal>
2020-04-04 18:50:33 +0000

    1586026233-autosave

--- yelpplant/s28/app.js
+++ yelpplant/s28/app.js
@@ -5,26 +5,31 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  { name: "salmon run", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg" },
+  { name: "graniteville", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg" },
+  { name: "mountain goat rest", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_1280.jpg"}
+];
+
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [
-            { name: "salmon run", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg" },
-            { name: "graniteville", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg" },
-            { name: "mountain goat rest", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_1280.jpg"}
-        ];
+
         
     res.render("campgrounds", {campgrounds : campgrounds});
 });
 
 app.get("/campgrounds/new", function(req, res) {
-    
+    res.render("new.ejs");
 });
 
 app.post("/campgrounds", function(req, res) {
-    
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
     
     res.redirect("/campgrounds");
 });
