8bf3c5f9c4a170fc627a6269c438e61f9379f1fb
Ubuntu <ubuntu@ip-172-31-32-5.us-east-2.compute.internal>
2020-04-04 17:56:36 +0000

    1586022996-autosave

---
+++ yelpplant/s28/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+    var campgrounds = [
+            { name: "salmon run", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg" },
+            { name: "graniteville", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg" },
+            { name: "mountain goat rest", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_1280.jpg"}
+        ];
+        
+    res.render("campgrounds", {campgrounds : campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    
+});
+
+app.post("/campgrounds", function(req, res) {
+    
+    
+    res.redirect("/campgrounds");
+});
+
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("server started " + process.env.IP + ":" + process.env.PORT);
+});
