6a4252707ac64d8c1561699ef15af31eced981a9
Ben Wallace <ben@benwal.io>
2020-04-05 13:35:09 -0400

    1586108109-autosave

---
+++ 30-310/app.js
@@ -0,0 +1,83 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/restful_blog");
+
+process.env.PORT = 3000;
+process.env.IP = "127.0.0.1";
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now()}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req,res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log("error - " + err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    })
+});
+
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, blog) {
+        if (err) {
+            res.render("new");
+            console.log("error - " + err);
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+});
+
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+app.get("/blogs/:id", function (req, res) {
+    Blog.findById(req.params.id, function(err, blog) {
+        if (err) {
+            console.log("error - " + err);
+            res.redirect("/");
+        } else {
+            res.render("show", {blog: blog});
+        }
+    })
+});
+
+app.get("/blogs/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, blog) {
+        if (err) {
+            console.log("error - " + err);
+            res.redirect("/");
+        } else {
+            res.render("edit", {blog: blog});
+        }
+    })
+});
+
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("server started " + process.env.IP + ":" + process.env.PORT);
+});
