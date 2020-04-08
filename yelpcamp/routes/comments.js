0d073c36fb8880b527251c549c12b9be815c8c76
Ben Wallace <ben@benwal.io>
2020-04-08 07:31:08 -0400

    1586345468-autosave

--- yelpcamp/routes/comments.js
+++ yelpcamp/routes/comments.js
@@ -19,7 +19,7 @@ router.get("/new", isLoggedIn, function (req, res) {
 });
 
 // comment create
-router.post("/comments", isLoggedIn, function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -32,7 +32,7 @@ router.post("/comments", isLoggedIn, function (req, res) {
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
                     comment.save();
-                    
+
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
