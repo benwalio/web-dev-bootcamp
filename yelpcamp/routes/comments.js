f7c5005d421c6db4c2cf0915f95f67209e6db60d
Ben Wallace <ben@benwal.io>
2020-04-09 09:17:01 -0400

    1586438221-autosave

--- yelpcamp/routes/comments.js
+++ yelpcamp/routes/comments.js
@@ -42,6 +42,33 @@ router.post("/", isLoggedIn, function (req, res) {
     });
 });
 
+router.get("/:comment_id/edit", function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, comment) {
+        if (err) {
+            console.log (err);
+        } else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: comment});
+        }
+    })
+});
+
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, comment) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+router.delete("/:comment_id",  function (req, res) {
+    Comment.findByIdAndDelete(req.params.comment_id, function (err) {
+        res.redirect("/campgrounds/" + req.params.id);
+    })
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
